6a1810b9dbcaa846586dbb81630676f315674019
lucas.hocquette <lucas.hocquette@student-cs.fr>
2024-12-16 15:28:09 +0100

    [TO TEST] Enable mulit-hyperparameter optimization
    Enabled hyperparameters are now : dense layer size, convolutional layer size, convolution kernel size

--- hpo.py
+++ hpo.py
@@ -6,18 +6,17 @@ class HyperParameterOptimizer:
     '''
     A hyper parameter optimizer class to optimize the hyperparameters of a simple CNN
     '''
-    def __init__(self, hyperparameter, hpspace, method, seed):
+    def __init__(self, hpspace, method, seed):
         '''
         Inputs :
-        - hyperparameter : the hyperparameter we want to optimize (char)
-        - hpspace : the possible values for the hyperparameter of study (numpy array)
+        - hpspace : the possible values for the hyperparameter of study (dict of numpy array)
         - method : the optimization method we want to use (seed)
         - seed : a seed model to start the optimization (torch.nn.Module)
         '''
-        self.hyperparameter = hyperparameter
         self.hpspace = hpspace
         self.method = method
         self.module = seed
+        self.seed = seed
         print("HyperParameterOptimizer initialized")
 
     def load_data(self):
@@ -38,9 +37,36 @@ class HyperParameterOptimizer:
         print("Data loaders have been initialized")
     
     def update_hyperparam(self, new_hp):
-        if self.hyperparameter == 'dense':
-            self.module.fc1.out_features = new_hp
-            self.module.fc2.in_features = new_hp
+        for hp in self.hyperparameter:
+            # Select the node to modify
+            node, next_node = self.fetch_node(hp)
+            if hp == 'F6':
+                node.out_features = new_hp[hp]
+                next_node.in_features = new_hp[hp]
+            elif hp in ['C1_chan','C3_chan','C5_chan']:
+                node.out_channel = new_hp[hp]
+                next_node.in_channel = new_hp[hp]
+            elif hp == ['C1_kernel','C3_kernel','C5_kernel']:
+                node.kernel_size = new_hp[hp]
+            pass
+
+    def fetch_node(self, hp):
+        if hp == 'F6':
+            node = self.module.fc1
+            next_node = self.module.fc2
+        elif hp in ['C1_chan','C1_kernel']:
+            node = self.module.conv1
+            next_node = self.module.conv2
+        elif hp in ['C3_chan','C3_kernel']:
+            node = self.module.conv2
+            next_node = self.module.conv3
+        elif hp in ['C5_chan','C5_kernel']:
+            node = self.module.conv5
+            next_node = self.module.fc1
+        else :
+            raise NameError('Model node unknown')
+        
+        return node, next_node
 
     def train_module(self,epochs):
         '''
@@ -54,13 +80,14 @@ class HyperParameterOptimizer:
 
         return acc
 
-    def optimize(self):
+    def optimize(self,**kwargs):
         if self.method == 'grid_search':
-            hpspace = self.hpspace.flatten()
+            hpspace = (dict(zip(self.hpspace.keys(), values)) for values in iter.product(*self.hpspace.values()))
             accuracy = np.zeros(hpspace.shape)
             epochs = 2
             
             for i in range(len(hpspace)):
+                self.module = self.seed # always start the optimization from the seed
                 self.update_hyperparam(hpspace[i])
                 accuracy[i] = self.train_module(epochs)
 
