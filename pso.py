d68d5a1649a6d02388a49a1ef666eb97b111bd57
lucas.hocquette <lucas.hocquette@student-cs.fr>
2024-12-22 19:50:18 +0100

    PSO subroutines debug

--- pso.py
+++ pso.py
@@ -4,11 +4,11 @@ import copy
 
 def initialize_swarm(S,hpspace):
     # Initialize the positions of the swarm
-    x_ind = np.random.choice(range(len(hpspace)),S)
+    x_ind = np.random.choice(range(len(hpspace)),S,replace=False)
     x = hpspace[x_ind]
 
     # Initialize at first with 0 speed
-    v = np.array([dict(zip(hpspace[0].keys,0))]*S)
+    v = np.array([{key:0 for key in hpspace[0].keys()}]*S)
 
     return Swarm(x),Swarm(v)
 
@@ -26,28 +26,28 @@ class Swarm:
     A Swarm class to perform classic algebraic operations on a swarm (pretty much )
     '''
     def __init__(self, particles):
-        Swarm.x = particles
+        self.x = particles
 
     def __add__(self,swarm):
-        if len(self.x)!=1 & len(swarm.x!=1):
-            assert len(self.x) == len(swarm.x), 'Swarms must have the same size'
+        if len(self)!=1 and len(swarm)!=1:
+            assert len(self) == len(swarm), 'Swarms must have the same size or should be broadcastable'
 
-            out = [{key:self.x[i][key]+swarm.x[i][key] for key in list(self.x.keys())} for i in range(len(self.x))]
-        elif len(self.x)!=1:
-            out = [{key:self.x[key]+swarm.x[i][key] for key in list(self.x.keys())} for i in range(len(swarm.x))]
+            out = [{key:self.x[i][key]+swarm.x[i][key] for key in list(self.keys())} for i in range(len(self))]
+        elif len(self.x)==1:
+            out = [{key:self.x[key]+swarm.x[i][key] for key in list(self.keys())} for i in range(len(swarm))]
         else :
             out = (swarm+self).x
         return Swarm(out)
     
     def __sub__(self,swarm):
-        if len(self.x)!=1 & len(swarm.x!=1):
+        if len(self)!=1 and len(swarm)!=1:
             assert len(self.x) == len(swarm.x), 'Swarms must have the same size'
 
-            out = [{key:self.x[i][key]-swarm.x[i][key] for key in list(self.x.keys())} for i in range(len(self.x))]
-        elif len(self.x)!=1:
-            out = [{key:self.x[key]-swarm.x[i][key] for key in list(self.x.keys())} for i in range(len(swarm.x))]
+            out = [{key:self.x[i][key]-swarm.x[i][key] for key in list(self.keys())} for i in range(len(self))]
+        elif len(self)==1:
+            out = [{key:self.x[key]-swarm.x[i][key] for key in list(self.keys())} for i in range(len(swarm))]
         else :
-            out = (swarm-self).x
+            out = [{key:self.x[i][key]-swarm.x[key] for key in list(self.keys())} for i in range(len(self))]
         return Swarm(out)
     
     def __mul__(self,lamda):
@@ -56,37 +56,78 @@ class Swarm:
         '''
         assert np.isscalar(lamda), "multiplying factor must be a scalar value"
 
-        out = [{key:self.x[i][key]*lamda for key in list(self.x.keys())} for i in range(len(self.x))]
+        out = [{key:self.x[i][key]*lamda for key in list(self.keys())} for i in range(len(self))]
         return Swarm(out)
     
     def __pow__(self,p):
         assert np.isscalar(p), "exponent must be scalar"
 
-        out = [{key:self.x[i][key]**p for key in list(self.x.keys())} for i in range(len(self.x))]
+        out = [{key:self.x[i][key]**p for key in list(self.keys())} for i in range(len(self))]
         return Swarm(out)
     
     def __len__(self):
+        if type(self.x) is dict:
+            return 1
         return len(self.x)
+
+    def __str__(self):
+        return str(self.x)
+    
+    def __getitem__(self,slice):
+        if type(self.x) is dict:
+            return self.x
+        return self.x[slice]
+    
+    def keys(self):
+        if type(self.x) is dict:
+            return self.x.keys()
+        return self.x[0].keys()
+    
+    def copy(self):
+        return Swarm(self.x.copy())
     
     def particular_norm(self):
-        norms = np.zeros_like(self.x)
-        for i in range(len(self.x)):
-            norms[i] = sum(np.array(list(self.x.values()))**2)
+        norms = np.zeros_like(self.x,dtype=np.float64)
+        for i in range(len(self)):
+            norms[i] = sum(np.array(list(self.x[i].values()))**2)
         
         return np.sqrt(norms)
     
     def global_norm(self):
         norms = self.particular_norm()
 
-        return np.sqrt(np.sum(norms**2))
+        return np.sqrt(np.sum(norms**2,dtype=np.float64))
     
     def round(self, space):
         '''
         Find the closest swarm configuration in a given space by euclidean norm
         '''
-        ind = np.zeros_like(self.x)
+        ind = np.zeros_like(self.x,dtype=np.int_)
         for i in range(len(self)):
-            dist = (self-space).particular_norm()
+            dist = (Swarm(self[i])-space).particular_norm()
             ind[i] = np.argmin(dist)
         
         return space[ind]
+    
+# A test script to validate the swarm object and the PSO functions
+if __name__ == '__main__':
+    particle1 = np.array([{'F6':80,'C3_chan':6},
+                          {'F6':60,'C3_chan':12}])
+    particle2 = np.array([{'F6':65,'C3_chan':13},
+                          {'F6':79,'C3_chan':6}])
+
+    sw1 = Swarm(particle1)
+    sw2 = Swarm(particle2)
+
+    # assert len(sw1)==2, f'wrong length, obtained {len(sw1)}'
+    # print(sw1.keys())
+    # print(sw1+sw2)
+    # print(sw1-sw2)
+    # print(sw1*3.5)
+    # print(sw1**2)
+    # print(sw1[1])
+    # print(sw1.particular_norm())
+    # print(sw1.global_norm())
+    print(sw1.round(sw2))
+
+    x,v = initialize_swarm(2,np.concatenate((particle1,particle2)))
