39e9a0dbea00ddeb0dd01b46004fab6dcfe815c8
lucas.hocquette <lucas.hocquette@student-cs.fr>
2024-12-25 19:26:15 +0100

    Improve epoch computation time with prefetch factor

--- LE_NET5_1.py
+++ LE_NET5_1.py
@@ -20,10 +20,10 @@ transform = transforms.Compose([
 def load_data():
     # This download the train and test data in CIFAR-10. They are not in the repositorie while they are stored in the folder data in the root
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=False, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True, num_workers=0, pin_memory=True)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True, num_workers=1, pin_memory=True, prefetch_factor=2)
 
     testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=False, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False, num_workers=0, pin_memory=True)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False, num_workers=1, pin_memory=True, prefetch_factor=2)
     return trainloader, testloader
 
 # Define the classes in CIFAR-10
